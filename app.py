d851f234447cefad80a7c275da4b8f5478904754
AI-Engineer2025 <AI-Engineer2025@github.com>
2025-08-27 16:53:12 +0200

    dtemplet und app angepasst

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+blog_posts =[
+    {"id": 1, "author": "John Doe", "title": "First Post", "content": "This is my first post."},
+    {"id": 2, "author": "Jane Doe", "title": "Second Post", "content": "This is another post."}
+    # More blog posts can go here...
+]
+
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html', posts=blog_posts)
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=True)
